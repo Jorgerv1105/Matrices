a2a94c6c05340b22b9b33ef19c6f89ebb217154e
Jorge <jorge.ramos.vasquez@udla.edu.ec>
2023-11-21 15:24:20 -0500

    Ingrese las matrices con las condiciones dadas

--- Matices.c
+++ Matices.c
@@ -12,25 +12,7 @@ int main(int argc, char const *argv[])
     printf("Ingrese el valor para columnas: ");
     scanf("%d", &m);
     //Agregamos la matriz con los casos de las fials y las columnas 
-    int matrix[n][m];
-    for (int i = 0; i < n; i++)
-    {
-        printf("\n");
-        for (int j = 0; j < m; j++)
-        {
-            //Colocamos la condicion para que la diagonal principal sea de 1
-            if (i == j)
-            {
-                matrix[i][j] = 1;
-            }
-            else
-            {
-                matrix[i][j] = 0;
-            }
-            //Imprimimos la matriz resultante 
-            printf("%d\t", matrix[i][j]);
-        }
-    }
+   
 
     return 0;
 }
